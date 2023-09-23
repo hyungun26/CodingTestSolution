de8cc7bb3d753cd0b79ed064e395ca8d95da63e2
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-09-23 11:18:40 +0900

    [level 2] Title: 피보나치 수, Time: 0.99 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int a = 0;
+        int b = 1;
+        for(int i = 1; i < n; i++)
+        {
+            answer = (a + b) % 1234567;
+            a = b;
+            b = answer;
+        }
+        return answer;
+    }
+}
