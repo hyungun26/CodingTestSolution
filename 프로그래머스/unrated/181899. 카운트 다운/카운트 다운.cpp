895b233bf298a41c5ad0fcc3eb2f829a9e23866c
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-09-25 18:01:48 +0900

    [unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int start, int end_num) {
+    vector<int> answer(start - end_num + 1);
+    for(int i = 0; i < answer.size(); i++)
+    {
+        answer[i] = start - i;
+    }
+    return answer;
+}
