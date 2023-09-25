d7a941943b1f3bfa8afd97f62eb8295abb04492c
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-09-26 00:17:49 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 3.28 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
@@ -1,25 +1,8 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.Length - 5];
-        List<int> list = new List<int>();
-        foreach(var i in num_list)
-        {
-            list.Add(i);
-        }
-        list.Sort();
-        
-        for(int i = 0; i < 5; i++)
-        {
-            list.RemoveAt(0);    
-        }
-        for(int i = 0; i < answer.Length; i++)
-        {
-            answer[i] = list[i];
-        }
-        
-        return answer;
+        return num_list.OrderBy(a => a).Skip(5).ToArray();
     }
 }
