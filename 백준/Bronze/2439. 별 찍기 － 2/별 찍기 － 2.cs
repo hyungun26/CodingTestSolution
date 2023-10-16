eba2daa0df96125535922bc2754b493e0a0ca67d
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-16 19:11:47 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 484 ms, Memory: 6576 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for(int j = 0; j < n-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
