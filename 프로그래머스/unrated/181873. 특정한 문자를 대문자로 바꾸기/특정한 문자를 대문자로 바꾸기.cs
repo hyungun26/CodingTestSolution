a1419378ff34bbfab99ea94a1fed64322a7eb1a6
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-16 17:38:51 +0900

    [level unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.75 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string my_string, string alp) {
+        string answer = my_string.Replace(alp, alp.ToUpper());
+        return answer;
+    }
+}
