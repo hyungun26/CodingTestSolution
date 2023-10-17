96dbd9b8fba508346cb044a58f069fc584025ada
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-17 12:10:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char c = s[0];
+            Console.WriteLine((int)c);
+        }
+    }
+}
