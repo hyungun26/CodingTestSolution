37bbe8c51cce74a7b838fdf16999133c35971389
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-18 14:50:51 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 64 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Baek11718
+{
+    static void Main()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input) == true) break;
+            Console.WriteLine(input);
+        }
+    }
+}
