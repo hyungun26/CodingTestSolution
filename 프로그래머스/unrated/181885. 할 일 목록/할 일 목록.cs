8a9f1bc4adb8702971c3272c5b99af7e54aef204
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-24 15:14:00 +0900

    [level unrated] Title: 할 일 목록, Time: 0.44 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string[] solution(string[] todo_list, bool[] finished) {
+        List<string> list = new List<string>();
+        for(int i = 0; i < todo_list.Length; i++)
+        {
+            if(!finished[i])
+                list.Add(todo_list[i]);
+        }
+        return list.ToArray();
+    }
+}
