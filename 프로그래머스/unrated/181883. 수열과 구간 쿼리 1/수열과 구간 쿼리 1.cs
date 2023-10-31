da37b22a4fb48001875a62a3b9cf0db0ced6ff55
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-31 16:19:09 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 0.45 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+        int[] answer = new int[] {};
+        for(int i = 0 ; i < queries.GetLength(0); i++)
+        {
+            for(int j = queries[i,0]; j <= queries[i,1]; j++)
+            {
+                arr[j] += 1;
+            }
+        }
+        return arr;
+    }
+}
