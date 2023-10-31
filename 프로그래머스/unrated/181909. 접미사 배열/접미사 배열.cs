7695403db3fcafd5b916f85470b77011f7c2f2eb
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-10-31 15:19:48 +0900

    [level unrated] Title: 접미사 배열, Time: 0.27 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public string[] solution(string my_string) {
+        string[] answer = new string[my_string.Length];
+        for(int i = 0; i < my_string.Length; i++)
+        {
+            answer[i] = my_string.Substring(my_string.Length-1-i, i+1);
+        }
+        Array.Sort(answer);
+        return answer;
+    }
+}
