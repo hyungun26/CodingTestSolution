9a90b53658b0933e0ecedac00c76bb353b432cb2
hyungun26 <104068067+hyungun26@users.noreply.github.com>
2023-11-07 02:17:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.07 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;
-        for(int i = 0; i < numbers.Length; i++){answer -= numbers[i];}
+        int answer = 0;
+        int[] iArr = {1,2,3,4,5,6,7,8,9};
+        answer = iArr.Except(numbers).Sum(); //(List, Array, Dictionary)간의 요소중에서 컬렉션에 중복으로 포함되는 요소를 제외하고 유일한 요소를 찾을때 사용한다 즉 예시 1번을 보면 1~9까지 숫자중 5와 9가 없다. iArr배열에 임시로 1~9까지 요소를 넣고 Except함수를 사용하면 numbers 배열와 비교를 했을때 없는 요소만 따로 찾을 수 있다.
             
         return answer;
     }
