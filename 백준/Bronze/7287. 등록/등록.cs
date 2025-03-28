175860498e4bf3f754062bd9f12ab8d7563ac793
hyeongeon <104068067+hyungun26@users.noreply.github.com>
2025-03-28 17:30:33 +0900

    [Bronze V] Title: 등록, Time: 44 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace baekjoon
+{
+    class baekjoon
+    {
+        static void Main()
+        {            
+            Console.WriteLine("90");
+            Console.Write("rebirth_hyungun");
+        }
+    }
+}
