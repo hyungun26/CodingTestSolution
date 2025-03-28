e3024b7bb2cdc85a1960ea3327335594f2db684a
hyeongeon <104068067+hyungun26@users.noreply.github.com>
2025-03-28 17:26:22 +0900

    [Bronze V] Title: 오늘 날짜, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace baekjoon
+{
+    class baekjoon
+    {
+        static void Main()
+        {            
+            Console.WriteLine("2025-03-28"); 
+        }
+    }
+}
