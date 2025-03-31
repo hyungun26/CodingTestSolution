5c791eea34584aa1b7c09d5a098f9aeb0ce9043f
hyeongeon <104068067+hyungun26@users.noreply.github.com>
2025-03-31 10:43:38 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace baekjoon
+{
+    class baekjoon
+    {
+        static void Main()
+        {
+            Console.WriteLine(ulong.Parse(Console.ReadLine()) * 4);
+        }
+    }
+}
