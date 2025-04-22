54e8266345cd931ca50d8b30c3137a318f4a41e2
hyeongeon <104068067+hyungun26@users.noreply.github.com>
2025-04-22 09:48:19 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace baejoon_1000
+{
+    class A_Plus_B
+    {        
+        static void Main()
+        {
+            int[] iArr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Console.WriteLine(iArr[0] + iArr[1]);
+        }
+    }
+}
